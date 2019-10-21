b94d69d3100ade0aadae22b6669444160ad40905
oldmo860617 <oldmo860617@gmail.com>
2019-10-21 12:54:33 +0800

    e2e-remote-server

--- trigger/index.js
+++ trigger/index.js
@@ -1,9 +1,40 @@
-// Include it and extract some methods for convenience
-const server = require('server');
+const server = require("server");
 const { get, post } = server.router;
+const admin = require("firebase-admin");
+let serviceAccount = require("./serviceAccountKey.json");
 
-// Launch server with options and a couple of routes
-server({ port: 8080 }, [
-  get('/', ctx => 'Hello world'),
-  post('/', ctx => console.log(ctx.data))
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://e2e-dino.firebaseio.com/"
+});
+
+let db = admin.database();
+let runnerRef = db.ref("/runner");
+let isRunningRef = db.ref("/isRunning");
+
+server({ port: 8082, security: { csrf: false } }, [
+  get("/", ctx => "Hello world!!"),
+  post("/", async ctx => {
+    let isRunning;
+    await isRunningRef.once("value", async snapshot => {
+      if (snapshot.val().isRunning == true) {
+        isRunning = true;
+      } else {
+        const data = ctx.data.text;
+        const dataList = data.split(" ");
+        await runnerRef.set({
+          branch: dataList[1],
+          type: dataList[0]
+        });
+        isRunning = false;
+        console.log(dataList);
+      }
+    });
+
+    if (isRunning === true) {
+      return "E2E server is running, please wait...";
+    } else {
+      return "change firebase data success";
+    }
+  })
 ]);
