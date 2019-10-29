19f8b950acdfb78a0858f435f986632ac29ade50
oldmo860617 <oldmo860617@gmail.com>
2019-10-29 16:21:25 +0800

    add yarn && pod install command

--- server.sh
+++ server.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 cd ~/desktop/verybuy-native
 git checkout $2
-
+yarn
+cd ios
+pod install
+cd ../
 if [ "$1" = "app" ]
 then
 	echo "Run app e2e"
     npx detox build -c ios.sim.debug
-	npx detox test -c ios.sim.debug 
+	npx detox test -c ios.sim.debug  e2e/scenarios/login/shoppingCart/categoryProducts/categoryAddToCart
 elif [ "$1" = "web" ]
 then
     echo "Run web e2e"
