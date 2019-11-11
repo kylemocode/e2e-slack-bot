09459ba1161453e4fa52d326bb0900fd861bc7c7
oldmo860617 <oldmo860617@gmail.com>
2019-11-11 14:42:57 +0800

    add android e2e command

--- server.sh
+++ server.sh
@@ -5,11 +5,16 @@ yarn
 cd ios
 pod install
 cd ../
-if [ "$1" = "app" ]
+if [ "$1" = "app/ios" ]
 then
-	echo "Run app e2e"
+	echo "Run app ios e2e"
     npx detox build -c ios.sim.debug
 	npx detox test -c ios.sim.debug
+elif [ "$1" = "app/android" ]
+then
+    echo "Run app android e2e"
+	npx detox build -c android.emu.debug
+	npx detox test -c android.emu.debug
 elif [ "$1" = "web" ]
 then
     echo "Run web e2e"
