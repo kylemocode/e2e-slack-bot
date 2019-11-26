f9cdce74753515f3dd616c69773c7501ac173d7e
oldmo860617 <oldmo860617@gmail.com>
2019-11-26 12:36:21 +0800

    add screen shot config

--- server.sh
+++ server.sh
@@ -10,12 +10,12 @@ then
 	pod install
 	cd ../
     npx detox build -c ios.sim.debug
-	npx detox test -c ios.sim.debug
+	npx detox test -c ios.sim.debug --record-videos failing --take-screenshots failing --cleanup
 elif [ "$1" = "app/android" ]
 then
     echo "Run app android e2e"
 	npx detox build -c android.emu.debug
-	npx detox test -c android.emu.debug
+	npx detox test -c android.emu.debug --record-videos failing --take-screenshots failing --cleanup
 elif [ "$1" = "web" ]
 then
     echo "Run web e2e"
